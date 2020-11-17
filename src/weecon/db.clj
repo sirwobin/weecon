709d68026d19f258a2147459061dfc877a47cc11
Robin Lunn <robin.lunn@booking.com>
2020-11-17 09:23:44 +0100

    reconcile! works now.

--- src/weecon/db.clj
+++ src/weecon/db.clj
@@ -11,9 +11,9 @@
 
 (defn- columns->sql-list [columns column_format_str]
   (->> (map #(format column_format_str %) columns)
-       (string/join ", ")))
+       (string/join ",")))
 
-(defn create-tables [{key-columns :key-columns value-columns :value-columns :as reconciliation-spec}]
+(defn create-tables! [{key-columns :key-columns value-columns :value-columns :as reconciliation-spec}]
   (let [create-table-sql-pattern (str "create table %s ("
                                       (string/join ", "
                                                    (flatten [(for [column-name key-columns]
@@ -22,7 +22,7 @@
                                                                (str column-name " TEXT NULL"))
                                                              (str "PRIMARY KEY (" (string/join "," key-columns) ")")]))
                                       ")")
-        recon-table-sql          (str "create table reconciliation ("
+        recon-table-sql          (str "create table reconciliation (weecon_action TEXT NOT NULL,"
                                       (string/join ", "
                                                    (flatten [(for [column-name key-columns]
                                                                (str column-name " TEXT NOT NULL"))
@@ -41,56 +41,77 @@
       (jdbc/execute! ds))
     (jdbc/execute! ds [recon-table-sql])))
 
-(defn import-csv [file-name table-name & [separator quote]]
+(defn import-csv! [file-name table-name & [separator quote]]
   (let [reader              (io/reader file-name)
         [header & contents] (csv/read-csv reader :separator (or separator \,) :quote (or quote \"))
         table               (keyword table-name)
         columns             (mapv keyword header)]
     (jdbc.sql/insert-multi! ds table columns contents)))
 
-(defn reconciliation [{key-columns :key-columns value-columns :value-columns :as reconciliation-spec}]
+(defn reconcile!
+  "I would normally perform the whole operation in one full outer join.  Since I have chosen sqlite
+   that is sadly not possible.  Instead I simulate a full outer join in three statements as per
+   https://www.sqlitetutorial.net/sqlite-full-outer-join/
+
+  This function inserts rows into the table named reconciliation as a side effect."
+  [{key-columns :key-columns value-columns :value-columns :as reconciliation-spec}]
   (let [key-columns-sql-no-prefix    (columns->sql-list key-columns "%s")
+        key-columns-sql-a-prefix     (columns->sql-list key-columns "a.%s")
         key-columns-sql-b-prefix     (columns->sql-list key-columns "b.%s")
+        value-columns-sql-a-prefix   (columns->sql-list value-columns "a.%s")
         value-columns-sql-b-prefix   (columns->sql-list value-columns "b.%s")
-        additions-sql                (str "INSERT INTO reconciliation ("
+        additions-sql                (str "INSERT INTO reconciliation (weecon_action,"
                                           key-columns-sql-no-prefix
                                           (when (seq value-columns) ",")
                                           (columns->sql-list value-columns "test_%s")
                                           ") SELECT 'added' as weecon_action, "
-                                          key-columns-sql-b-prefix
-                                          ", "
-                                          value-columns-sql-b-prefix
+                                          key-columns-sql-a-prefix
+                                          ","
+                                          (columns->sql-list value-columns "a.%s")
                                           " FROM test a LEFT OUTER JOIN authority b USING ("
                                           key-columns-sql-no-prefix
                                           ") WHERE b."
                                           (first key-columns)
                                           " IS NULL")
-        deletions-sql                (str "INSERT INTO reconciliation ("
+        deletions-sql                (str "INSERT INTO reconciliation (weecon_action,"
                                           key-columns-sql-no-prefix
                                           (when (seq value-columns) ",")
                                           (columns->sql-list value-columns "authority_%s")
                                           ") SELECT 'deleted' as weecon_action, "
-                                          key-columns-sql-b-prefix
+                                          key-columns-sql-a-prefix
                                           ", "
-                                          value-columns-sql-b-prefix
+                                          value-columns-sql-a-prefix
                                           " FROM authority a LEFT OUTER JOIN test b USING ("
                                           key-columns-sql-no-prefix
                                           ") WHERE b."
                                           (first key-columns)
                                           " IS NULL")
-        changes-sql                  (str "INSERT INTO reconciliation ("
-                                          key-columns-sql-no-prefix
-                                          (when (seq value-columns) ",")
-                                          (columns->sql-list value-columns "authority_%s")
-                                          (when (seq value-columns) ",")
-                                          (columns->sql-list value-columns "test_%s")
-                                          ") SELECT 'changed' as weecon_action, "
-                                          key-columns-sql-b-prefix
-                                          (when (seq value-columns) ",")
-                                          (columns->sql-list value-columns "a.%1$s as authority_%1$s")
-                                          (when (seq value-columns) ",")
-                                          (columns->sql-list value-columns "b.%1$s as test_%1$s"))])
-  (jdbc.sql/query ds))
+        changes-sql                  (when (seq value-columns)  ; if there are no value columns then there can, by definition, be no changes.  Only additions or deletions.
+                                       (str "INSERT INTO reconciliation (weecon_action,"
+                                            key-columns-sql-no-prefix
+                                            ","
+                                            (columns->sql-list value-columns "authority_%s")
+                                            ","
+                                            (columns->sql-list value-columns "test_%s")
+                                            ") SELECT 'changed' as weecon_action, "
+                                            key-columns-sql-b-prefix
+                                            ","
+                                            (columns->sql-list value-columns "a.%1$s as authority_%1$s")
+                                            ","
+                                            (columns->sql-list value-columns "b.%1$s as test_%1$s")
+                                            " FROM authority a JOIN test b USING ("
+                                            key-columns-sql-no-prefix
+                                            ") WHERE "
+                                            (string/join " OR "
+                                                         (map #(format "a.%1$s <> b.%1$s" %) value-columns))))]
+    (doseq [stmt (remove nil? [additions-sql deletions-sql changes-sql])]
+      (jdbc/execute! ds [stmt]))))
+
+(defn tt []
+  (reconcile!     {:authority     {:file-name "config-examples/authority.csv" :file-type "csv" :column-names "header row"}
+                   :test          {:file-name "config-examples/test.csv" :file-type "csv" :column-names "header row"}
+                   :key-columns   ["name" "id_number"]
+                   :value-columns ["age" "height"]}))
 
 (comment
   (ns-unmap (find-ns 'weecon.db) 'ds)
@@ -109,10 +130,14 @@ FROM   test a
   LEFT OUTER JOIN authority b USING (name, id_number)
 WHERE  b.name IS NULL
   "
-  (create-tables {:authority     {:file-name "config-examples/authority.csv" :file-type "csv" :column-names "header row"}
-                  :test          {:file-name "config-examples/test.csv" :file-type "csv" :column-names "header row"}
-                  :key-columns   ["name" "id_number"]
-                  :value-columns ["age" "height"]})
-  (import-csv "config-examples/authority.csv" "authority")
-  (import-csv "config-examples/test.csv" "test")
-  (columns->sql-list ["name" "id_number"] "b.%s"))
+  (create-tables! {:authority     {:file-name "config-examples/authority.csv" :file-type "csv" :column-names "header row"}
+                   :test          {:file-name "config-examples/test.csv" :file-type "csv" :column-names "header row"}
+                   :key-columns   ["name" "id_number"]
+                   :value-columns ["age" "height"]})
+  (import-csv! "config-examples/authority.csv" "authority")
+  (import-csv! "config-examples/test.csv" "test")
+  (columns->sql-list ["name" "id_number"] "b.%s")
+  (reconcile!     {:authority     {:file-name "config-examples/authority.csv" :file-type "csv" :column-names "header row"}
+                   :test          {:file-name "config-examples/test.csv" :file-type "csv" :column-names "header row"}
+                   :key-columns   ["name" "id_number"]
+                   :value-columns ["age" "height"]}))
